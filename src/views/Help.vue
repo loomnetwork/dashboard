09a5d5ddb486b6e273a532d3c28b6884d91165ee
tharamalai <57070054@kmitl.ac.th>
2019-01-28 12:21:00 +0700

    Add Help Component

---
+++ src/views/Help.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="faucet-with-navbar">
+    <div class="faucet-content">
+      <div>
+        <h1 class="page-header">Help</h1>
+      </div>
+      <div class="help-block">
+        <h3 class="question">What are gateways?</h3>
+        <p>Gateways is ...</p>
+      </div>
+      <div class="help-block">
+        <h3 class="question">Questions</h3>
+        <p>Answer</p>
+      </div>
+      <div class="help-block">
+        <h3 class="question">Questions</h3>
+        <p>Answer</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '~@/assets/scss/_mixin.scss';
+@import '~@/assets/scss/_variables.scss';
+
+.page-header {
+  margin-top: 24px;
+  margin-bottom: 32px;
+}
+
+.question {
+  color: gray
+}
+
+.help-block{
+  margin: 10px 0px 10px 0px;
+  padding: 2px;
+}
+
+</style>
+

faa0ab4dfec53892dcb63edea55121a5a39a5bc8
Jim Eric Skogman <yim.apichai@gmail.com>
2019-02-21 15:01:21 +0700

    Add button check

--- src/views/ValidatorDetail.vue
+++ src/views/ValidatorDetail.vue
@@ -51,9 +51,7 @@
                 <b-tooltip target="delegateBtn" placement="bottom" title="Transfer tokens to this validator"></b-tooltip>
                 <b-button id="undelegateBtn" class="px-5 py-2 mx-3" variant="primary" @click="openRequestUnbondModal" :disabled="!canDelegate || !hasDelegation || delegationState != 'Bonded'">Un-delegate</b-button>
                 <b-tooltip target="undelegateBtn" placement="bottom" title="Withdraw your delegated tokens"></b-tooltip>
-                <!-- TODO: Uncomment -->
-                <!-- :disabled="!hasDelegation || !canDelegate || (delegationState != 'Bonded' && amountDelegated != 0)" -->
-                <b-button id="redelegateBtn" class="px-5 py-2" variant="primary" @click="openRedelegateModal">Redelegate</b-button>
+                <b-button id="redelegateBtn" class="px-5 py-2" variant="primary" @click="openRedelegateModal" :disabled="!hasDelegation || !canDelegate || (delegationState != 'Bonded' && amountDelegated != 0)">Redelegate</b-button>
                 <b-tooltip target="redelegateBtn" placement="bottom" title="Redelegate from/to another delegator"></b-tooltip>
               </div>
             </div>

cc944f49c016fdb7aae6d860e25a47649375c5d5
Jim Eric Skogman <yim.apichai@gmail.com>
2019-02-28 19:53:40 +0700

    Query events through web3

--- src/views/History.vue
+++ src/views/History.vue
@@ -14,40 +14,95 @@
   import Vue from 'vue'
   import { Component } from 'vue-property-decorator'
   import FaucetTable from '../components/FaucetTable'
+  import { mapGetters, mapState, mapActions, mapMutations, createNamespacedHelpers } from 'vuex'
+
+  import { formatToCrypto } from '@/utils'
+
+  const DappChainStore = createNamespacedHelpers('DappChain')
+  const DPOSStore = createNamespacedHelpers('DPOS')
 
   @Component({
     components: {
       FaucetTable
+    },
+    computed: {
+     ...DappChainStore.mapState([
+       "web3",
+       "GatewayInstance"
+     ]),
+     ...DPOSStore.mapState([
+       "currentMetamaskAddress"
+     ])
     }
   })
   export default class History extends Vue {
 
-  history = [
-    {
-      ID: 2349134,
-      Date: "3/14/18",
-      Amount: "10000 LOOM",
-      To: "trdzvzr7...",
-      From: "Me"
-    },
-    {
-      ID: 2349134,
-      Date: "3/14/18",
-      Amount: "10000 LOOM",
-      To: "trdzvzr7...",
-      From: "Me"
-    },
-    {
-      ID: 2349134,
-      Date: "3/14/18",
-      Amount: "10000 LOOM",
-      To: "trdzvzr7...",
-      From: "Me"
+    // "ERC20Received" = Deposit?
+    // "TokenWithdrawn" = Withdraw?
+
+    history = [
+      {
+        ID: 2349134,
+        Date: "3/14/18",
+        Amount: "10000 LOOM",
+        To: "trdzvzr7...",
+        From: "Me"
+      },
+      {
+        ID: 2349134,
+        Date: "3/14/18",
+        Amount: "10000 LOOM",
+        To: "trdzvzr7...",
+        From: "Me"
+      },
+      {
+        ID: 2349134,
+        Date: "3/14/18",
+        Amount: "10000 LOOM",
+        To: "trdzvzr7...",
+        From: "Me"
+      }
+    ]
+
+    mounted() {
+      this.queryEvents()
     }
-  ]
 
+    queryEvents() {
+      if(!this.web3 ||
+         !this.GatewayInstance ||
+         !this.currentMetamaskAddress) return
 
-  }</script>
+      this.web3.eth.getBlockNumber().then((latestBlockNumber) => {
+        let filter = { from: this.currentMetamaskAddress }
+        return this.GatewayInstance.getPastEvents(
+          "allEvents",
+          {
+            filter,
+            fromBlock: latestBlockNumber-10000,
+            toBlock: latestBlockNumber
+          }
+        )
+      }).then((events) => {
+        let results = events.filter((event) => {
+          return event.returnValues.from === "0x78577310f2E6BC94989d0899ad00CC016E4D6050" 
+        }).map((event) => {
+          return {
+            "Block #" : event.blockNumber,
+            "Event"   : event.event,
+            "Amount"  : formatToCrypto(event.returnValues.amount),
+            "Tx Hash" : event.transactionHash
+           }
+        })
+        this.history = results        
+      }).catch((err) => {
+        console.log("Error querying events", err)
+      })
+
+    }
+  }
+  
+  </script>
 
 <style lang="scss" scoped>
   .dropdown-container {

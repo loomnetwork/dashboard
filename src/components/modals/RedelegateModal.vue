7e71dd460dc977d7b22e2a6775d9d228a9d6d38d
Jim Eric Skogman <yim.apichai@gmail.com>
2019-03-04 18:41:07 +0700

    Fix one liner fix for redelegateAsync

--- src/components/modals/RedelegateModal.vue
+++ src/components/modals/RedelegateModal.vue
@@ -110,7 +110,7 @@ export default class RedelegateModal extends Vue {
     }
 
 
-    await redelegateAsync(this.origin.pubKey, this.target.pubKey, this.delegation.amount)
+    await this.redelegateAsync(this.origin.pubKey, this.target.pubKey, this.delegation.amount)
 
     // this.$emit("ok")
     this.$refs.modalRef.hide()

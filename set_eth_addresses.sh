661e4a2356ab9a589b063252913f6931d3d07437
Georgios Konstantopoulos <me@gakonst.com>
2019-03-29 16:34:04 +0200

    fix sed script

--- set_eth_addresses.sh
+++ set_eth_addresses.sh
@@ -1,3 +1,2 @@
-
-sed -i '' 's/LOOM_ADDRESS = "[^"]*"/LOOM_ADDRESS = "'$LOOM_ADDRESS'"/' src/store/dappChainStore.js; 
-sed -i '' 's/GW_ADDRESS = "[^"]*"/GW_ADDRESS = "'$GW_ADDRESS'"/' src/store/dappChainStore.js;
+sed -i 's/LOOM_ADDRESS = "[^"]*"/LOOM_ADDRESS = "'$LOOM_ADDRESS'"/' src/store/dappChainStore.js; 
+sed -i 's/GW_ADDRESS = "[^"]*"/GW_ADDRESS = "'$GW_ADDRESS'"/' src/store/dappChainStore.js;

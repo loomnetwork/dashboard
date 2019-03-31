d6c7ff85f49f7829ca6955985c002cef1e1beed5
Reda <reda.bendiar@gmail.com>
2019-03-31 18:14:49 +0700

    default macos sed fails

--- set_eth_addresses.sh
+++ set_eth_addresses.sh
@@ -1,2 +1,13 @@
-sed -i 's/LOOM_ADDRESS = "[^"]*"/LOOM_ADDRESS = "'$LOOM_ADDRESS'"/' src/store/dappChainStore.js; 
-sed -i 's/GW_ADDRESS = "[^"]*"/GW_ADDRESS = "'$GW_ADDRESS'"/' src/store/dappChainStore.js;
+TARGET='src/store/dappChainStore.js';
+
+normal_sed() {
+    sed -i 's/LOOM_ADDRESS = "[^"]*"/LOOM_ADDRESS = "'$LOOM_ADDRESS'"/' $TARGET; 
+    sed -i 's/GW_ADDRESS = "[^"]*"/GW_ADDRESS = "'$GW_ADDRESS'"/' $TARGET;
+}
+darwin_sed() {
+    echo $TARGET;
+    sed -i '' 's/LOOM_ADDRESS = "[^"]*"/LOOM_ADDRESS = "'$LOOM_ADDRESS'"/' $TARGET; 
+    sed -i '' 's/GW_ADDRESS = "[^"]*"/GW_ADDRESS = "'$GW_ADDRESS'"/' $TARGET;
+}
+
+normal_sed || darwin_sed;

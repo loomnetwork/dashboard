a0873d2d7372cb81ce92b480a572257cef8e89d5
Jim Eric Skogman <yim.apichai@gmail.com>
2019-04-22 17:41:38 +0700

    Add seperate config file

---
+++ chain-config.ts
@@ -0,0 +1,26 @@
+export default {
+  "plasma": {
+    "dappchainEndpoint" : "https://plasma.dappchains.com",
+    "mainnetEndpoint" : "https://mainnet.infura.io",
+    "chainId" : "default",
+    "gatewayAddress" : "0x8f8e8b3c4de76a31971fe6a87297d8f703be8570"
+  },
+  "asia1": {
+    "dappchainEndpoint" : "https://test-z-asia1.dappchains.com",
+    "mainnetEndpoint" : "https://rinkeby.infura.io",
+    "chainId" : "default",
+    "gatewayAddress" : "0x76c41eFFc2871e73F42b2EAe5eaf8Efe50bDBF73"
+  },
+  "us1": {
+    "dappchainEndpoint" : "http://test-z-us1.dappchains.com",
+    "mainnetEndpoint" : "http://rinkeby.infura.io",
+    "chainId" : "default",
+    "gatewayAddress" : "0x76c41eFFc2871e73F42b2EAe5eaf8Efe50bDBF73"
+  },
+  "local": {
+    "dappchainEndpoint" : "http://test-z-us1.dappchains.com",
+    "mainnetEndpoint" : "https://rinkeby.infura.io",
+    "chainId" : "default",
+    "gatewayAddress" : "0x76c41eFFc2871e73F42b2EAe5eaf8Efe50bDBF73"
+  }
+}

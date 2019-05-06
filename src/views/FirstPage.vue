a366daa7e334587fb39ae7b71a7ca566b1e40a52
Luke Zhang <luke@lukezhang.ca>
2019-05-06 17:32:17 +0800

    add trezor support via metamask

--- src/views/FirstPage.vue
+++ src/views/FirstPage.vue
@@ -32,7 +32,17 @@
                     or extension                      
                   </small>
                 </b-card>                  
-              </div>                
+              </div>      
+               <div class="col-sm-12 col-md-6">
+                <b-card class="wallet-selection-card text-center" @click="selectWallet('metamask')">
+                  <h5>Trezor <small>via Metamask</small></h5>
+                  <img src="../assets/trezor_logo.png">
+                  <small>
+                    Connect to your Trezor wallet <br>
+                    via Metamask                    
+                  </small>
+                </b-card>                  
+              </div>                 
             </div>  
           </b-card>
 

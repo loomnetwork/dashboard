bdd0af2d711976807b8cc39a648921cbbf935d27
Nutchy <nutchy@loomx.io>
2019-05-23 17:35:40 +0700

    Add the feedback form

--- src/views/FeedbackForm.vue
+++ src/views/FeedbackForm.vue
@@ -1,60 +1,16 @@
 <template>
   <div id="feedback-form">
     <b-card class="mb-4 form-contianer" no-body>
-      <h4>Feedback Form</h4>
-      <b-form>
-        <b-form-group label="Email address:" label-for="email-input">
-          <b-form-input id="email-input" v-model="form.email" type="email" placeholder="Your email address"></b-form-input>
-        </b-form-group>
-        <b-form-group label="Title:" label-for="input-2">
-          <b-form-input id="title-input" v-model="form.title" placeholder="Title"></b-form-input>
-        </b-form-group>
-        <b-row class="my-1">
-          <b-col sm="6">
-            <b-form-group label="Category:" label-for="input-3">
-              <b-form-select id="input-3" :options="form.categories" v-model="form.catSelected"></b-form-select>
-            </b-form-group>
-          </b-col>
-          <b-col sm="6">
-            <b-form-group label="Wallet:" label-for="input-3">
-              <b-form-select id="input-3" :options="form.wallets" v-model="form.walletSelected"></b-form-select>
-            </b-form-group>
-          </b-col>
-        </b-row>
-        <b-form-group label="Eth Address:" label-for="input-4">
-          <b-form-input id="eth-addrs-input" placeholder="Eth Address" v-model="form.ethAddress"></b-form-input>
-        </b-form-group>
-        <b-row class="my-1">
-          <b-col sm="6">
-            <b-form-group label="Platform:" label-for="input-5">
-          <b-form-select id="input-3" :options="form.platforms" v-model="form.platformSelected"></b-form-select>
-        </b-form-group>
-          </b-col>
-          <b-col sm="6">
-            <b-form-group label="Browser:" label-for="input-6">
-              <b-form-input id="input-3" placeholder="e.g. Chrome" v-model="form.browser"></b-form-input>
-            </b-form-group>
-          </b-col>
-        </b-row>
-        <b-form-group label="Error Message:" label-for="input-7">
-          <b-form-textarea id="input-3" rows="4" v-model="form.errorMsg"></b-form-textarea>
-        </b-form-group>
-        <b-form-group label="Screenshot:" label-for="input-8">
-          <b-form-file
-            accept="image/*"
-            v-model="form.file"
-            :state="Boolean(file)"
-            placeholder="Choose a file..."
-            drop-placeholder="Drop file here..."
-          ></b-form-file>
-        </b-form-group>
-        <b-form-group label="Additional Context:" label-for="input-8">
-          <b-form-textarea id="input-3" rows="4" v-model="form.additionalContext"></b-form-textarea>
-        </b-form-group>
-        <div class="to-right">
-          <b-button @click="onSubmit" variant="primary" size="lg">Submit</b-button>
-        </div>
-      </b-form>
+      <!-- For Enabling CAPTCHA: add "&captcha=yes" in src property -->
+      <iframe title="Feedback Form" 
+        class="freshwidget-embedded-form"
+        id="freshwidget-embedded-form" 
+        src="https://loom.freshdesk.com/widgets/feedback_widget/new?&widgetType=embedded&formTitle=Contact+Support&submitTitle=Send+to+Support&submitThanks=Thanks!+Your+message+has+been+added+to+our+support+queue.+One+of+our+agents+will+get+back+to+you+within+the+next+2+work+days.&searchArea=no&captcha=yes" 
+        scrolling="no"
+        height="1100px"
+        width="100%"
+        frameborder="0" >
+      </iframe>
     </b-card>
   </div>
 </template>

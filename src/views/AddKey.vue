4a35d9af1b03456fda54f2e3610c7188f1b9f0a5
amliomnie <57070145@kmitl.ac.th>
2019-05-24 18:57:14 +0700

    change tier to radio button

--- src/views/AddKey.vue
+++ src/views/AddKey.vue
@@ -5,7 +5,7 @@
       <b-card v-for="pk in publicKeys" :key="pk.Hex" v-show="publicKeys.length > 0">
         <b-row>
           <b-col cols="12" sm="6">
-            <b-form-input v-model="pk[pk.defaultFormat]"></b-form-input>
+            <h6>{{pk[pk.defaultFormat] | loomAddress}}</h6>
           </b-col>
           <b-col cols="12" sm="3">
             <b-button @click="switchPubKeyType(pk)"> View {{pk.defaultFormat | swapTextBase64AndHexLabel}}</b-button>
@@ -25,21 +25,28 @@
       </b-row>
       <div role="group">
         <label for="input-live">Your Loom Public Address</label>
-        <b-form-input v-model="newPubKey" class="my-2" placeholder="0x0000000000000000000"></b-form-input>
+        <b-form-input v-model="newPubKey" class="my-2" placeholder="loom0000000000000000000"></b-form-input>
       </div>
       <p @click="generateNewPublicKey()" class="text-right text-link"> Generate New Public Address</p><br>
-      Amount to Stake
-      <b-row class="mt-3">
-        <b-col v-for="tier in stakeTiers" class="mt-3 mb-3" :key="tier.no">
-          <b-button @click="addKey(tier)"> 
-            Tier: {{tier.no}} <br> 
-            Max: {{tier.max}} tx/min <br>
-            Amount: {{tier.amount}} LOOM 
-          </b-button>
-        </b-col>
-      </b-row>
       <generate-public-key-modal :showModal="isShowGenPublicKeyModal" @hide="onHideGenPKModal" />
-      <div class="remaining">
+      <label for="input-live"> Amount to Stake </label>
+        <b-form-radio-group
+          id="btn-radios-2"
+          v-model="tierSelected"
+          :options="stakeTiers"
+          buttons
+          button-variant="outline-secondary"
+          size="lg"
+          name="radio-btn-outline"
+        ></b-form-radio-group>
+      <div class="my-3" v-show="tierSelected != ''">
+        <b-row><label class="my-2" for="input-live"> Tier Detail </label></b-row>
+        <b-row><label for="input-live"> Tier: {{tierSelected.no}} </label></b-row>
+        <b-row><label for="input-live"> Max: {{tierSelected.max}} </label></b-row>
+        <b-row><label for="input-live"> Amount: {{tierSelected.amount}} LOOM </label></b-row>
+      </div>
+      <b-button class="d-inline-flex" @click="addKey(tierSelected)"> Add Key </b-button>
+      <div class="remaining my-3">
         <span class="text-right"> Remaining Balance: {{ loomBalance }} LOOM (</span>
         <router-link class="text-right" :to='{name :"account", query: { action: "deposit" } }'>deposit</router-link>
         <span class="text-right">)</span> 
@@ -86,39 +93,73 @@ export default class AddKey extends Vue {
       tier: 1,
       defaultFormat: "Hex",
     },
+  ] // TODO: wait for the real data
+
+  tierSelected = ''
+
+   stakeTiers = [
+    {
+      text: "Tier1: 3000 LOOM",
+      value: {
+        no: 1,
+        max: 10,
+        amount: 3000,
+      },
+    },
+    {
+      text: "Tier2: Coming soon",
+      value: {
+        no: 2,
+        max: 20,
+        amount: 6000,
+      },
+      disabled: true,
+    },
+    {
+      text: "Tier3: Coming soon",
+      value: {
+        no: 3,
+        max: 30,
+        amount: 9000,
+      },
+      disabled: true,
+    },
   ]
 
-  stakeTiers = [
-    {
-      no: 1,
-      max: 10,
-      amount: 3000,
-    },
-    {
-      no: 2,
-      max: 20,
-      amount: 6000,
-    },
-    {
-      no: 3,
-      max: 30,
-      amount: 9000,
-    },
-  ]
+  // stakeTiers = [
+  //   {
+  //     no: 1,
+  //     max: 10,
+  //     amount: 3000,
+  //   },
+  //   {
+  //     no: 2,
+  //     max: 20,
+  //     amount: 6000,
+  //   },
+  //   {
+  //     no: 3,
+  //     max: 30,
+  //     amount: 9000,
+  //   },
+  // ] // TODO: wait for the real data
 
   switchPubKeyType(inputKey) {
     inputKey.defaultFormat = inputKey.defaultFormat === "Base64" ? "Hex" : "Base64"
   }
 
-   addKey(tier) {
-    alert("DONE")
+  addKey(tier) {
+    alert(tier)
   }
-   generateNewPublicKey() {
+
+  generateNewPublicKey() {
     this.isShowGenPublicKeyModal = true
   }
+
   onHideGenPKModal() {
     this.isShowGenPublicKeyModal = false
   }
+
   async mounted() {
     this.loomBalance = await this.getDappchainLoomBalance()
   }

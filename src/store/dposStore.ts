04836beb0ef09d6e0622b5a5738bd16b7d35b216
Georgios Konstantopoulos <me@gakonst.com>
2019-05-30 09:20:43 -0400

    check if limbo delegation hasn't been claimed, and if so, claim it

--- src/store/dposStore.ts
+++ src/store/dposStore.ts
@@ -483,6 +483,11 @@ export default {
       const user:DPOSUserV3 = await rootState.DappChain.dposUser
       try {
         debug("claimDelegatorRewardsAsync")
+        const limboValidator = "0x00000000000000000000000000000000"
+        const limboDelegations = await user.checkDelegationsAsync(limboValidator)
+        if (limboDelegations!.delegationsArray.length > 0) {
+          await user.undelegateAsync(limboValidator, 0, 0) 
+        }
         await user.claimDelegatorRewardsAsync()
       } catch(err) {
         console.error(err)

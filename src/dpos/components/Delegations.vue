e88e0871b5551dc73297d4adade3b7830f08651b
Reda <reda.bendiar@gmail.com>
2019-06-04 13:07:49 +0700

    move dpos components to /dpos

--- src/dpos/components/Delegations.vue
+++ src/dpos/components/Delegations.vue
@@ -6,8 +6,10 @@
         <dd>{{delegation.state | delegationState}}</dd>
         <dt>{{ $t('views.validator_detail.amount_delegated') }}</dt>
         <dd>{{delegation.amount | tokenAmount}}</dd>
-        <dt>{{ $t('views.validator_detail.updated_amount') }}</dt>
-        <dd>{{delegation.updateAmount | tokenAmount}}</dd>
+        <template v-if="delegation.updateAmount.gt(zero)">
+          <dt>{{ $t('views.validator_detail.updated_amount') }}</dt>
+          <dd>{{delegation.updateAmount | tokenAmount}}</dd>
+        </template>
         <dt>{{ $t('views.validator_detail.timelock_tier') }}</dt>
         <dd>{{delegation.lockTimeTier | lockTimeTier}}</dd>
         <dt>Unlock time</dt>
@@ -34,25 +36,22 @@
 import { Vue, Component, Prop } from "vue-property-decorator"
 import { DPOSState, HasDPOSState } from "@/store/dpos/types"
 import { Store } from "vuex"
-import { dposModule } from "../../store/dpos"
+import { dposModule } from "@/store/dpos"
+import { ZERO } from '../../utils';
+import { Delegation } from 'loom-js/dist/proto/dposv3_pb';
 
 @Component
 export default class DelegationsList extends Vue {
 
+  zero = ZERO
   /**
    * Filter by validator
    */
-  @Prop({ default: "" })
-  validatorAddress!: string
+  @Prop({ required: true })
+  delegations!: Delegation[]
 
   get state(): DPOSState {
     return (this.$store as Store<HasDPOSState>).state.dpos
-  }
-
-  get delegations() {
-    return this.validatorAddress === "" ?
-      this.state.delegations :
-      this.state.delegations.filter((d) => d.validator.local.toString() === this.validatorAddress)
   }
 
   requestRedelegation(delegation) {

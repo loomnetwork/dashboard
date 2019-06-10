d3db7c55e10aef6bd1a76c2901e0347290526a02
Reda <reda.bendiar@gmail.com>
2019-06-10 11:41:10 +0700

    fix ElectionTimer import

--- src/views/MobileAccount.vue
+++ src/views/MobileAccount.vue
@@ -209,7 +209,7 @@ import { DashboardState } from "../types"
 import { ethereumModule } from "../store/ethereum"
 import { plasmaModule } from "../store/plasma"
 import { dposModule } from "@/dpos/store"
-import ElectionTimer from "@/dpos/components/electionTimer.vue"
+import ElectionTimer from "@/dpos/components/ElectionTimer.vue"
 
 const log = debug("mobileaccount")
 

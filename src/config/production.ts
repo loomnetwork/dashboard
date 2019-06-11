d3c1fa46d5d335d077d4ae2b3c340c81e746e663
amliomnie <57070145@kmitl.ac.th>
2019-06-11 12:45:49 +0700

    remove comment

--- src/config/production.ts
+++ src/config/production.ts
@@ -11,7 +11,7 @@ export default {
     chainId: "eth",
     endpoint: "wss://mainnet.infura.io/ws",
     etherScan: "https://etherscan.com",
-    blockExplorer: "http://plasma-blockexplorer.dappchains.com", //update this to correct one
+    blockExplorer: "http://plasma-blockexplorer.dappchains.com",
     contracts: {
       gateway: "0xE57e0793f953684Bc9D2EF3D795408afb4a100c3",
       loomGateway: "0x76c41effc2871e73f42b2eae5eaf8efe50bdbf73",

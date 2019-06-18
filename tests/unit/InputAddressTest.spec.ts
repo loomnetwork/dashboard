bbf0538d380a346e2dc2d09ad405eebe0878d4f0
Jaab <jaab@loomx.io>
2019-06-18 18:10:31 +0700

    Add test cases for address pattern any

--- tests/unit/InputAddressTest.spec.ts
+++ tests/unit/InputAddressTest.spec.ts
@@ -10,6 +10,7 @@ const factory = (props = {}) => {
 
 const LOOM_CHAIN = "loom"
 const BNB_CHAIN = "bnb"
+const ANY_CHAIN = "any"
 const LOOM_ADDRESS = "looma777afeF41c00b2570A9ac3878F5D92e838f3371"
 const BNB_ADDRESS = "0xa777afeF41c00b2570A9ac3878F5D92e838f3371"
 
@@ -23,6 +24,20 @@ describe("InputAddress: Valid address cases", () => {
 
   it("Should not show an error message when bnb address is valid", () => {
     const wrapper = factory({ value: "", chain: BNB_CHAIN })
+    // @ts-ignore
+    wrapper.vm.onInput(BNB_ADDRESS)
+    expect(wrapper.find(".error").exists()).to.equal(false)
+  })
+
+  it("Should not show an error message when any address is valid", () => {
+    const wrapper = factory({ value: "", chain: ANY_CHAIN })
+    // @ts-ignore
+    wrapper.vm.onInput(LOOM_ADDRESS)
+    expect(wrapper.find(".error").exists()).to.equal(false)
+  })
+
+  it("Should not show an error message when any address is valid", () => {
+    const wrapper = factory({ value: "", chain: ANY_CHAIN })
     // @ts-ignore
     wrapper.vm.onInput(BNB_ADDRESS)
     expect(wrapper.find(".error").exists()).to.equal(false)
@@ -56,6 +71,13 @@ describe("InputAddress: Invalid address cases", () => {
     expect(wrapper.find(".error").text()).to.equal(`Invalid ${LOOM_CHAIN} address.`)
   })
 
+  it("Should show an error message when any address has more than 44 char", () => {
+    const wrapper = factory({ value: "", chain: ANY_CHAIN })
+    // @ts-ignore
+    wrapper.vm.onInput("looma777afeF41c00b2570A9ac3878F5D92e838f33710")
+    expect(wrapper.find(".error").text()).to.equal(`Invalid ${ANY_CHAIN} address.`)
+  })
+
   it("Should show an error message when loom address has less than 44 char", () => {
     const wrapper = factory({ value: "", chain: LOOM_CHAIN })
     // @ts-ignore
@@ -63,25 +85,46 @@ describe("InputAddress: Invalid address cases", () => {
     expect(wrapper.find(".error").text()).to.equal(`Invalid ${LOOM_CHAIN} address.`)
   })
 
+  it("Should show an error message when any address has less than 44 char", () => {
+    const wrapper = factory({ value: "", chain: ANY_CHAIN })
+    // @ts-ignore
+    wrapper.vm.onInput("looma777afeF41c00b2570A9ac3878F5D92e838f337")
+    expect(wrapper.find(".error").text()).to.equal(`Invalid ${ANY_CHAIN} address.`)
+  })
+
   it("Should show an error message when bnb address is invalid", () => {
-    const wrapper = factory({ value: "", chain: BNB_ADDRESS })
+    const wrapper = factory({ value: "", chain: BNB_CHAIN })
     // @ts-ignore
     wrapper.vm.onInput("0Xa777afeF41c00b2570A9ac3878F5D92e838f3371")
-    expect(wrapper.find(".error").text()).to.equal(`Invalid ${BNB_ADDRESS} address.`)
+    expect(wrapper.find(".error").text()).to.equal(`Invalid ${BNB_CHAIN} address.`)
   })
 
-  it("Should show an error message when bnb address has more than 44 char", () => {
-    const wrapper = factory({ value: "", chain: BNB_ADDRESS })
+  it("Should show an error message when bnb address has more than 42 char", () => {
+    const wrapper = factory({ value: "", chain: BNB_CHAIN })
     // @ts-ignore
     wrapper.vm.onInput("0xa777afeF41c00b2570A9ac3878F5D92e838f33710")
-    expect(wrapper.find(".error").text()).to.equal(`Invalid ${BNB_ADDRESS} address.`)
+    expect(wrapper.find(".error").text()).to.equal(`Invalid ${BNB_CHAIN} address.`)
+  })
+  
+  it("Should show an error message when any address has more than 42 char", () => {
+    const wrapper = factory({ value: "", chain: ANY_CHAIN })
+    // @ts-ignore
+    wrapper.vm.onInput("0xa777afeF41c00b2570A9ac3878F5D92e838f33710")
+    expect(wrapper.find(".error").text()).to.equal(`Invalid ${ANY_CHAIN} address.`)
   })
 
-  it("Should show an error message when bnb address has less than 44 char", () => {
-    const wrapper = factory({ value: "", chain: BNB_ADDRESS })
+  it("Should show an error message when bnb address has less than 42 char", () => {
+    const wrapper = factory({ value: "", chain: BNB_CHAIN })
     // @ts-ignore
     wrapper.vm.onInput("0xa777afeF41c00b2570A9ac3878F5D92e838f337")
-    expect(wrapper.find(".error").text()).to.equal(`Invalid ${BNB_ADDRESS} address.`)
+    expect(wrapper.find(".error").text()).to.equal(`Invalid ${BNB_CHAIN} address.`)
+  })
+
+  it("Should show an error message when any address has less than 44 char", () => {
+    const wrapper = factory({ value: "", chain: ANY_CHAIN })
+    // @ts-ignore
+    wrapper.vm.onInput("0xa777afeF41c00b2570A9ac3878F5D92e838f337")
+    expect(wrapper.find(".error").text()).to.equal(`Invalid ${ANY_CHAIN} address.`)
   })
 })
 

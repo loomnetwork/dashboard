1dec6364e93c83ae290b22a27b909adb2c48c753
Jim Eric Skogman <yim.apichai@gmail.com>
2019-06-26 14:27:34 +0700

    Move change of config to dev

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -24,6 +24,7 @@ export default {
   dpos: {
     bootstrapNodes: [],
   },
+  coinDataUrl: "https://dev-auth.loom.games/wallet/tokens",
   gateway: {
     chains: ["ethereum"],
     multisig: false,

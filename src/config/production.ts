99e350b4fada2ad97ab1c3cc481ca94e4bba7d9e
Jim Eric Skogman <yim.apichai@gmail.com>
2019-06-26 18:29:32 +0700

    Allow deposit and withdraw of eth

--- src/config/production.ts
+++ src/config/production.ts
@@ -36,6 +36,6 @@ export default {
     multisig: false,
   },
   coinDataUrl: "https://auth.loom.games/wallet/tokens",
-  disabled: ["transfer-asset", "binance", "dev-deploy", "eth-withdraw", "eth-deposit"],
+  disabled: ["transfer-asset", "binance", "dev-deploy"],
   chains: ["ethereum"],
 } as DashboardConfig

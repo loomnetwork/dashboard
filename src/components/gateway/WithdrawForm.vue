057d7957e9555a16ccd2c1e5469a378050dafa3f
Jim Eric Skogman <yim.apichai@gmail.com>
2019-07-04 19:05:54 +0700

    Add bech32 dependency and convert to uint8

--- src/components/gateway/WithdrawForm.vue
+++ src/components/gateway/WithdrawForm.vue
@@ -14,14 +14,21 @@
         :min="min"
         :max="userBalance"
         :symbol="token"
+        :round="false"
         v-model="weiAmount"
         @isError="errorHandler"
       />
     </div>
-    <div v-if="token === 'BNB'">
+    <div v-if="token === 'BNB'" class="mt-3">
       <h6>Recepient</h6>
       <h6>Your address on Binance chain (DEX)</h6>
-      <input type="text" v-model="recepient">
+      <input-address
+        v-model="recepient"
+        chain="binance"
+        :blacklist="[ownAddress]"
+        :placeholder="'Binance (DEX) address'"
+        @isValid="isValidAddressFormat"
+      />
     </div>
     <template slot="modal-footer">
       <b-btn @click="close()">Cancel</b-btn>
@@ -39,23 +46,27 @@
 <script lang="ts">
 import Vue from "vue"
 import BN from "bn.js"
+import bech32 from "bech32"
 import { Component, Prop } from "vue-property-decorator"
 import { ethers } from "ethers"
 
 import { formatTokenAmount } from "@/filters"
-import { formatToCrypto } from "@/utils"
+import { formatToCrypto, formatToLoomAddress } from "@/utils"
 import { DashboardState, Funds } from "../../types"
 import { gatewayModule } from "../../store/gateway"
 import { gatewayReactions } from "../../store/gateway/reactions"
 
 import AmountInput from "@/components/AmountInput.vue"
+import InputAddress from "../InputAddress.vue"
 import { plasmaWithdraw } from "../../store/gateway/plasma"
 import { setShowWithdrawProgress } from "../../store/gateway/mutations"
-import { LocalAddress, Address } from 'loom-js';
+import { LocalAddress, Address, CryptoUtils } from 'loom-js';
+import { AddressInfo } from 'net';
 
 @Component({
   components: {
     AmountInput,
+    InputAddress,
   },
 })
 
@@ -66,6 +77,7 @@ export default class WithdrawForm extends Vue {
   weiAmount: BN = new BN(0)
   min = new BN(1)
   amountIsValid: boolean = false
+  isValidAddress: Address | null = null
   recepient = ""
 
   setShowWithdrawForm = gatewayModule.setShowWithdrawForm
@@ -80,6 +92,10 @@ export default class WithdrawForm extends Vue {
     return this.state.plasma.coins[this.token].balance
   }
 
+  get ownAddress() {
+    return formatToLoomAddress(this.state.plasma.address.toLowerCase())
+  }
+
   get transferRequest() {
     return this.state.gateway.transferRequest
   }
@@ -87,13 +103,18 @@ export default class WithdrawForm extends Vue {
   get visible() {
     return this.transferRequest.type === "WITHDRAW"
       && this.transferRequest.token !== ""
-      && this.transferRequest.chain === "ethereum"
+      && (this.transferRequest.chain === "ethereum"
+      || this.transferRequest.chain === "binance")
   }
 
   set visible(value) {
     if (value === false) {
       gatewayModule.clearTransferRequest()
     }
+  }
+
+  isValidAddressFormat(isValid) {
+    return this.isValidAddress = isValid
   }
 
   close() {
@@ -104,18 +125,27 @@ export default class WithdrawForm extends Vue {
     this.amountIsValid = isValid
   }
 
+  decodeAddress(value) {
+    const decodeAddress = bech32.decode(value)
+    return Buffer.from(bech32.fromWords(decodeAddress.words))
+  }
+
   async requestWithdrawHandler(e) {
     e.preventDefault()
 
     // TODO: Cache transactions
     // const txObj = this.state.gateway.pendingTransactions[0]
-    const recepientAddr = new Address("binance", LocalAddress.fromHexString(this.recepient))
+    let address = new Address("", new LocalAddress(new Uint8Array()))
+    if (this.isValidAddress && this.recepient) {
+      const tmp = this.decodeAddress(this.recepient)
+      address = new Address("binance", new LocalAddress(tmp))
+    }
 
     const payload: Funds = {
-      chain: "ethereum",
+      chain: this.transferRequest.chain,
       symbol: this.transferRequest.token,
       weiAmount: this.weiAmount,
-      recepient: recepientAddr,
+      recepient: address,
     }
 
     this.beginWithdrawal(payload)

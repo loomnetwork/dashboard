8560102290034828db8ecfbbda3048d5d3631857
Jaab <jaab@loomx.io>
2019-07-31 18:05:08 +0700

    add loom deposit test

---
+++ cypress/integration/deposit.spec.js
@@ -0,0 +1,48 @@
+describe('Deposit Test', () => {
+
+  before(() => {
+    cy.visit('https://localhost:8080')
+
+    cy.contains('Metamask (Test Wallet)').click()
+
+    cy.get('#faucet-sidebar')
+      .contains('Deposit/Withdraw').click()
+  })
+
+  it('Depositing 0.1 LOOM', () => {
+    cy.contains('.symbol', 'LOOM')
+      .parent()
+      .as('token')
+
+    cy.get('@token')
+      .find('.balance')
+      .first()
+      .as('balance')
+
+    cy.get('@balance').then(($span) => {
+      const expectedBalance = parseFloat($span.text()) + 0.1
+
+      cy.get('@token')
+        .contains('Deposit').click()
+  
+      cy.get('input').type('0.1')
+
+      cy.contains('Confirm').click()
+
+      cy.contains('OK').click()
+
+      cy.wait(300000)
+
+      cy.get('#faucet-header')
+        .should(
+          'contain',
+          "The deposit has been confirmed. Your plasma balance will be updated after 10 confirmations."
+        )
+
+      cy.get('@balance').then(($span) => {
+        const actualBalance = parseFloat($span.text())
+        expect(actualBalance).to.equal(expectedBalance)
+      })
+    })
+  })
+})

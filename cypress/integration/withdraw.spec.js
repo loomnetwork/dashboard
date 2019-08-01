377bef2a2a0e11901331be10f11117cadc86c265
Jaab <jaab@loomx.io>
2019-08-01 15:59:49 +0700

    add eth withdrawal test

--- cypress/integration/withdraw.spec.js
+++ cypress/integration/withdraw.spec.js
@@ -56,4 +56,51 @@ describe('Withdrawal Test', () => {
       cy.get('@expectedBalance').should('eq', actualBalance)
     })
   })
+
+  it('Withdrawing 1 ETH', () => {
+    cy.contains('.symbol', 'ETH')
+      .parent()
+      .as('token')
+
+    cy.get('@token')
+      .find('.balance')
+      .first()
+      .as('balance')
+
+    cy.get('@balance').then(($span) => {
+      const expectedBalance = parseFloat($span.text()) - 1
+      cy.wrap(expectedBalance).as('expectedBalance')
+    })
+
+    cy.get('@token')
+      .contains('Withdraw').click()
+
+    cy.get('input').type('1')
+
+    cy.get('#deposit-approval-success button')
+      .contains('Withdraw').click()
+
+    cy.get('#withdraw-confirmed p', { timeout: 30000 })
+      .should(
+        "contain",
+        "1 ETH have been withdrawn out of Plasmachain. " +
+        "Click complete to transfer your funds to your ethereum account."
+      )
+
+    cy.get('#withdraw-confirmed button')
+      .contains('Complete withdraw').click()
+
+    cy.get('#faucet-header', { timeout: 30000 })
+      .should(
+        'contain',
+        "Transaction sent successfully."
+      )
+
+    cy.get('.close').click()
+
+    cy.get('@balance').then(($span) => {
+      const actualBalance = parseFloat($span.text())
+      cy.get('@expectedBalance').should('eq', actualBalance)
+    })
+  })
 })

f0fb0a4241077b9d86e4260e84ae095a50b1c949
Jaab <jaab@loomx.io>
2019-08-01 15:59:17 +0700

    add loom withdrawal test

---
+++ cypress/integration/withdraw.spec.js
@@ -0,0 +1,59 @@
+describe('Withdrawal Test', () => {
+
+  before(() => {
+    cy.visit('https://localhost:8080')
+
+    cy.contains('Metamask (Test Wallet)').click()
+
+    cy.get('#faucet-sidebar')
+      .contains('Deposit/Withdraw').click()
+  })
+
+  it('Withdrawing 1 LOOM', () => {
+    cy.contains('.symbol', 'LOOM')
+      .parent()
+      .as('token')
+
+    cy.get('@token')
+      .find('.balance')
+      .first()
+      .as('balance')
+
+    let aaa
+    cy.get('@balance').then(($span) => {
+      const expectedBalance = parseFloat($span.text()) - 1
+      cy.wrap(expectedBalance).as('expectedBalance')
+    })
+
+    cy.get('@token')
+      .contains('Withdraw').click()
+
+    cy.get('input').type('1')
+
+    cy.get('#deposit-approval-success button')
+      .contains('Withdraw').click()
+
+    cy.get('#withdraw-confirmed p', { timeout: 30000 })
+      .should(
+        "contain",
+        "1 LOOM have been withdrawn out of Plasmachain. " +
+        "Click complete to transfer your funds to your ethereum account."
+      )
+
+    cy.get('#withdraw-confirmed button')
+      .contains('Complete withdraw').click()
+
+    cy.get('#faucet-header', { timeout: 30000 })
+      .should(
+        'contain',
+        "Transaction sent successfully."
+      )
+
+    cy.get('.close').click()
+
+    cy.get('@balance').then(($span) => {
+      const actualBalance = parseFloat($span.text())
+      cy.get('@expectedBalance').should('eq', actualBalance)
+    })
+  })
+})

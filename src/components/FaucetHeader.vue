9a9a7110787e11ffb53f7c075f517c3115e9cac5
Jim Eric Skogman <yim.apichai@gmail.com>
2019-08-19 16:20:04 +0700

    Fix typo and style images

--- src/components/FaucetHeader.vue
+++ src/components/FaucetHeader.vue
@@ -181,6 +181,10 @@ export default class FaucetHeader extends Vue {
       ],
       help: [
         {
+          to: "/staking-guide",
+          text: "components.faucet_sidebar.staking_guide",
+        },
+        {
           to: "/faq",
           text: "components.faucet_sidebar.faq",
         },

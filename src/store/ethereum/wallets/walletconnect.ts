d787018846813a2c7c9123d30185f46559a61762
Reda <reda.bendiar@gmail.com>
2019-08-30 14:00:59 +0700

    Wallet Connect, update to latest beta

--- src/store/ethereum/wallets/walletconnect.ts
+++ src/store/ethereum/wallets/walletconnect.ts
@@ -1,7 +1,5 @@
-import { ethers } from "ethers"
 import { WalletType, EthereumConfig } from "../types"
 
-import Web3 from "web3"
 import WalletConnectProvider from "@walletconnect/web3-provider"
 
 export const WalletConnectAdapter: WalletType = {
@@ -19,13 +17,16 @@ export const WalletConnectAdapter: WalletType = {
     const net = config.networkId === "1" ? "mainnet" : "rinkeby"
 
     const provider = new WalletConnectProvider({
-      bridge: "https://bridge.walletconnect.org" // Required
+      bridge: "https://bridge.walletconnect.org",
+      // infuraId: "5Ic91y0T9nLh6qUg33K0", // Required
+      infuraId: "44ad2034a545495ca22dda2a4d50feba",
     })
+    provider.on("chainChanged", (chainId) => console.log("chainChanged", chainId))
+    provider.on("networkChanged", (net) => console.log("networkChanged", net))
+    provider.on("accountsChanged", (acc) => window.location.reload())
 
-    return new Promise((resolve, reject) => {
-      resolve(provider)
-    })
-
+    await provider.enable()
+    return provider
   },
 
 }

a6d75030fab28e533af211567f32de5e78e111df
Napong T <58070033@kmitl.ac.th>
2019-09-26 16:48:25 +0700

    Add more bounties

--- src/views/BountyPage.vue
+++ src/views/BountyPage.vue
@@ -20,11 +20,11 @@
       <h2 class="bounty-topic mb-3">{{ topic }}</h2>
       <b-card class="mb-3" v-for="(bountyData, index) in bounty" :key="index">
         <b-row>
-          <b-col sm>
+          <b-col sm="8">
             <b-card-title>{{ bountyData.title }}</b-card-title>
             <p>{{ bountyData.description }}</p>
           </b-col>
-          <b-col sm class="bounty-reward">
+          <b-col sm="4" class="bounty-reward">
             {{ loomAmount(bountyData.reward) | tokenAmount(18,0)}} {{ bountyData.token }}
           </b-col>
         </b-row>

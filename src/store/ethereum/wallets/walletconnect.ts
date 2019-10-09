deb90143dfdd61151dbab5fbb703511bb65a589c
Napong Tansakul <58070033@kmitl.ac.th>
2019-10-09 17:47:00 +0700

    Reload a page when user cancel a QR Scan #1204 (#1255)

--- src/store/ethereum/wallets/walletconnect.ts
+++ src/store/ethereum/wallets/walletconnect.ts
@@ -1,6 +1,21 @@
 import { WalletType, EthereumConfig } from "../types"
 
 import WalletConnectProvider from "@walletconnect/web3-provider"
+import WalletConnectQRCodeModal from "@walletconnect/qrcode-modal"
+
+const INITIAL_STATE = {
+  walletConnector: null,
+  fetching: false,
+  connected: false,
+  chainId: 1,
+  showModal: false,
+  pendingRequest: false,
+  uri: "",
+  accounts: [],
+  address: "",
+  result: null,
+  assets: [],
+}
 
 export const WalletConnectAdapter: WalletType = {
   id: "walletconnect",
@@ -24,9 +39,17 @@ export const WalletConnectAdapter: WalletType = {
     provider.on("chainChanged", (chainId) => console.log("chainChanged", chainId))
     provider.on("networkChanged", (net) => console.log("networkChanged", net))
     provider.on("accountsChanged", (acc) => window.location.reload())
+    // wc is WalletConnect class in library
+    provider.wc.on('disconnect', (error, payload) => {
+      if (error) {
+        throw error
+      }
+      console.log("User cancel a sign-in request")
+      WalletConnectQRCodeModal.close()
+      provider.updateState({...INITIAL_STATE})
+    })
 
     await provider.enable()
     return provider
   },
-
 }

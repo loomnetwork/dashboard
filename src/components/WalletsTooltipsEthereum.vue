6dca577b6e2ba827bbc537d64e9d89e176cd47b3
Reda <reda.bendiar@gmail.com>
2021-05-05 16:20:15 +0700

    extract tooltips

---
+++ src/components/WalletsTooltipsEthereum.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <b-popover
+      class="popover"
+      target="metamask-button"
+      title="Metamask"
+      triggers="hover"
+    >
+      <p>
+        This wallet also supports <strong>Ledger</strong> and
+        <strong>Trezor</strong>.
+      </p>
+      <p>
+        If you want to use a Ledger or Trezor open the Metamask extension and
+        select
+        <strong>Connect Hardware Wallet</strong> from the menu, then follow the
+        steps to setup your device for use with Metamask.
+      </p>
+      <img
+        style="width: 100%"
+        src="@/assets/metamask-hardware-screencap.png"
+        alt="Screenshot of setting up hardware wallet in Metamask"
+      />
+    </b-popover>
+    <b-popover
+      class="popover"
+      target="eth-walletconnect-button"
+      title="WalletConnect Protocol"
+      triggers="hover"
+    >
+      WalletConnect supports many <strong>iOS</strong> and
+      <strong>Android</strong> wallets. It's easy to use and there's nothing to
+      install, just scan a QR code to connect.
+    </b-popover>
+    <b-popover
+      class="popover"
+      target="eth-walletlink-button"
+      title="WalletLink/Coinbase"
+      triggers="hover"
+    >
+      For the time being WalletLink only supports Coinbase Wallet mobile app.
+    </b-popover>
+  </div>
+</template>
+<style scoped>
+.popover-body img {
+  width: 100%;
+}
+</style>

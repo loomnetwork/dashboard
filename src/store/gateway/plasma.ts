7317acf5a5deb6df9c93f70205cf8a7ee312f131
mimi <mimiheadmeng@gmail.com>
2021-06-18 11:44:01 +0700

    using gateway error codes for catch error

--- src/store/gateway/plasma.ts
+++ src/store/gateway/plasma.ts
@@ -334,8 +334,10 @@ export async function plasmaWithdraw(context: ActionContext, funds: Funds) {
       return
     }
     feedback.endTask()
-    if (error.message.includes("limit reached")) {
-      feedback.showError(i18n.t("feedback_msg.error.withdraw_failed_limit_reached").toString())
+    if (error.message.includes("TG024")) {
+      feedback.showError(i18n.t("feedback_msg.error.withdraw_failed_total_limit_reached").toString())
+    } else if (error.message.includes("TG025")) {
+      feedback.showError(i18n.t("feedback_msg.error.withdraw_failed_account_limit_reached").toString())
     } else {
       feedback.showError(i18n.t("feedback_msg.error.withdraw_failed").toString())
     }

8ccb07ac6f1c20295fb13cda3483ea409389a318
mimi <mimiheadmeng@gmail.com>
2021-06-21 19:37:45 +0700

    display valid sum of each delegation tiers

--- src/dpos/components/ValidationDelegations.vue
+++ src/dpos/components/ValidationDelegations.vue
@@ -1,51 +1,103 @@
 <template>
-  <b-card class="mb-4">
-      <b-card-body >
-        <!-- <b-list-group-item v-for="usersAirdrop in usersAirdrops" :key="usersAirdrop.airdropID">
-        <dl>
-            <dt>{{$t('components.validation_delegations.airdrop_amount')}}:</dt>
-            <dd>{{usersAirdrop.airdropAmount | tokenAmount(getTokensInfo(usersAirdrop.tokenAddress).decimals)}}</dd>
-            <dt>{{$t('components.validation_delegations.token')}}:</dt>
-            <dd>{{getTokensInfo(usersAirdrop.tokenAddress).name}}</dd>
-            <dt>{{$t('components.validation_delegations.unlock_time')}}:</dt>
-            <dd>{{usersAirdrop.timelock | date('seconds')}}</dd>
-        </dl>
+  <div class="validation-delegations">
+    <!-- <b-card class="mb-4"> -->
+    <h6>Total stake: {{ totalStaked | tokenAmount(18, 0) }} LOOM</h6>
+    <div role="tablist" v-if="delegations.length">
+      <div v-for="(tier, index) in locktimeTiers">
         <b-button
           block
-          variant="outline-primary"
-          type="button"
-          @click="withdraw(usersAirdrop.airdropID)"
-          :disabled="isOntimeLocked(usersAirdrop) || usersAirdrop.isWithdrew"
+          :aria-expanded="visible ? 'true' : 'false'"
+          aria-controls="collapse-4"
+          @click="visible = !visible"
+          class="my-2"
         >
-        {{$t('components.validation_delegations.withdraw')}}
+          <dl v-if="">
+            <dt>Locktime {{ tier }}</dt>
+            <dd>
+              {{
+                getSumDelegations(getDelegationsTier(index)) | tokenAmount
+              }}
+              LOOM
+            </dd>
+            <!-- getDelegationsTier(index) -->
+          </dl>
         </b-button>
-        </b-list-group-item> -->
-      </b-card-body>
-  </b-card>
+        <!-- <b-collapse id="collapse-4" v-model="visible" class="mt-2">
+          <b-card
+            v-for="(delegation, i) in delegations"
+            :key="delegation.unlockTime"
+            >I should start open!
+
+            <dl>
+              <dt>Tier {{ i }}</dt>
+              <dd>{{ delegation.amount | tokenAmount }} LOOM</dd>
+            </dl>
+          </b-card>
+        </b-collapse> -->
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator"
-import { DPOSState, HasDPOSState, Delegation, Validator } from "@/dpos/store/types"
-import { Store } from "vuex"
-import { dposModule } from "@/dpos/store"
+
+import { Delegation, HasDPOSState, Validator } from "@/dpos/store/types"
 import { ZERO } from "@/utils"
-import { plasmaModule } from "@/store/plasma"
+import { formatTokenAmount } from "@/filters"
 
-@Component
+@Component({
+  components: {},
+})
 export default class ValidationDelegations extends Vue {
-
   zero = ZERO
+  visible = true
   /**
    * Filter by validator
    */
   @Prop({ required: true }) validator!: Validator
+  // delegations = []
 
-
-  get state(): DPOSState {
-    return (this.$store as Store<HasDPOSState>).state.dpos
+  get state(): HasDPOSState {
+    return this.$store.state
   }
 
+  get totalStaked() {
+
+    // console.log("this.state.dpos.delegations,",this.state.dpos);
+    // console.log("this.validator.delegations,",this.validator);
+    return this.validator.totalStaked
+  }
+  summm = ZERO
+
+  get delegations() {
+    
+    // return this.state.dpos.delegations.filter((d) => d.validator.addr === this.validator!.addr)
+    return this.validator.allDelegations
+  }
+
+  getDelegationsTier(tierIndex: number) {
+    return this.delegations.filter((d) => d.lockTimeTier === tierIndex)
+  }
+
+  getSumDelegations(delegations: Delegation[]) {
+    let sum = ZERO
+    delegations.forEach((delegation) => {
+      sum = sum.add(delegation.amount)
+    })
+    return sum
+  }
+
+  get locktimeTiers() {
+    return [
+      this.$t("components.modals.faucet_delegate_modal.two_weeks").toString(),
+      this.$t(
+        "components.modals.faucet_delegate_modal.three_months"
+      ).toString(),
+      this.$t("components.modals.faucet_delegate_modal.six_months").toString(),
+      this.$t("components.modals.faucet_delegate_modal.one_year").toString(),
+    ]
+  }
 }
 </script>
 <style lang="scss" scoped>
